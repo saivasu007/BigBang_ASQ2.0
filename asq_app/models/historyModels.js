653229f14727d639f16d1aa62dc1ba5cbc73c144
thungasr <89589.thungathurti@students.itu.edu>
2015-11-06 17:54:56 -0800

    Committed for ASQ Upgrade2.0
    ASQ Models part of 1.0 changed by adding more fields and Created new
    model for multiple certifications.

--- asq_app/models/historyModels.js
+++ asq_app/models/historyModels.js
@@ -5,7 +5,9 @@ var HistorySchema = new mongoose.Schema({
     email: String,
     mode: String,
     date: String,
+    time: String,
     score: Number,
+    category: String,
     epWrong: Number,
     gkWrong: Number,
     maWrong: Number,
@@ -20,6 +22,13 @@ var HistorySchema = new mongoose.Schema({
     scmNumber: Number,
     sqmNumber: Number,
     svvNumber: Number,
+    epScore: Number,
+    gkScore: Number,
+    maScore: Number,
+    pmScore: Number,
+    scmScore: Number,
+    sqmScore: Number,
+    svvScore: Number,
     total: Number,
     report: Object
 });
--- asq_app/models/userModel.js
+++ asq_app/models/userModel.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var mongoose = require('mongoose');
 
+//Updated by Srinivas Thungathurti for ASQ Upgrade 2.0
 var userSchema = new mongoose.Schema({
     email: String,
-    passwd1: String,
+    password: String,
     firstName: String,
-    lastName: String
+    lastName: String,
+    address1: String,
+    address2: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    role: String,
+    activeIn: String,
+    expiryDate: String,
+    subscriber: String,
+    birthDate: String
 });
+
+userSchema.methods.validPassword = function( pwd ) {
+    return ( this.password === pwd );
+};
+
+//End Changes for ASQ Upgrade 2.0 here.
 
 module.exports = mongoose.model('userModel', userSchema);
